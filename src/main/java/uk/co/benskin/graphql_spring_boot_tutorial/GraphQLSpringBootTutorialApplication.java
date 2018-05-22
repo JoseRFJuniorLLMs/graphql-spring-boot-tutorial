3f24e38699f240fbedcb7afeca7a886ed834c3b1
Sam Benskin <1180089+sambenskin@users.noreply.github.com>
2018-05-22 07:01:20 +0100

    Adding main spring boot application file

---
+++ src/main/java/uk/co/benskin/graphql_spring_boot_tutorial/GraphQLSpringBootTutorialApplication.java
@@ -0,0 +1,12 @@
+package uk.co.benskin.graphql_spring_boot_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQLSpringBootTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQLSpringBootTutorialApplication.class, args);
+    }
+}
