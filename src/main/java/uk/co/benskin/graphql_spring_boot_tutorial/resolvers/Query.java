e44a113a14dd3afc3c29db5dbfadc947fa47325a
Sam Benskin <1180089+sambenskin@users.noreply.github.com>
2018-06-05 17:03:45 +0100

    Changing the query to get pet data from our database through our new repository

--- src/main/java/uk/co/benskin/graphql_spring_boot_tutorial/resolvers/Query.java
+++ src/main/java/uk/co/benskin/graphql_spring_boot_tutorial/resolvers/Query.java
@@ -1,23 +1,20 @@
 package uk.co.benskin.graphql_spring_boot_tutorial.resolvers;
 
-import java.util.ArrayList;
-import java.util.List;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
 import uk.co.benskin.graphql_spring_boot_tutorial.entities.Pet;
-import uk.co.benskin.graphql_spring_boot_tutorial.enums.Animal;
+import uk.co.benskin.graphql_spring_boot_tutorial.repositories.PetRepository;
 
 @Component
 public class Query implements GraphQLQueryResolver {
 
-    public List<Pet> pets() {
-        List<Pet> pets = new ArrayList<>();
-        Pet aPet = new Pet();
-        aPet.setId(1l);
-        aPet.setName("Bill");
-        aPet.setAge(9);
-        aPet.setType(Animal.MAMMOTH);
-        pets.add(aPet);
-        return pets;
+    private PetRepository PetRepository;
+
+    public Query(PetRepository PetRepository) {
+        this.PetRepository = PetRepository;
+    }
+
+    public Iterable<Pet> pets() {
+        return PetRepository.findAll();
     }
 }
