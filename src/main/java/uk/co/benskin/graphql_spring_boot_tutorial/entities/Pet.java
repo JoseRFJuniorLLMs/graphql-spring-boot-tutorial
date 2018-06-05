37c957a0fdcb2bf39fd63b1fd9701085685affed
Sam Benskin <1180089+sambenskin@users.noreply.github.com>
2018-06-05 17:02:47 +0100

    Adding JPA annotations to our entity for persistence to the database

--- src/main/java/uk/co/benskin/graphql_spring_boot_tutorial/entities/Pet.java
+++ src/main/java/uk/co/benskin/graphql_spring_boot_tutorial/entities/Pet.java
@@ -1,14 +1,27 @@
 package uk.co.benskin.graphql_spring_boot_tutorial.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 import uk.co.benskin.graphql_spring_boot_tutorial.enums.Animal;
 
 @Data
+@Entity
+@Table(name="pets")
 public class Pet {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
 
     private String name;
-
+    
+    @Enumerated(EnumType.STRING)
     private Animal type;
 
     private int age;
