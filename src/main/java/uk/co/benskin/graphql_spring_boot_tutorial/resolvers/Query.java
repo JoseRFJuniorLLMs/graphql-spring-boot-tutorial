67d4d4434e31de728f552b963018d375d9b70d69
Sam Benskin <1180089+sambenskin@users.noreply.github.com>
2018-06-06 08:18:37 +0100

    Adding required args constructor from lombok

--- src/main/java/uk/co/benskin/graphql_spring_boot_tutorial/resolvers/Query.java
+++ src/main/java/uk/co/benskin/graphql_spring_boot_tutorial/resolvers/Query.java
@@ -2,17 +2,16 @@ package uk.co.benskin.graphql_spring_boot_tutorial.resolvers;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
 import uk.co.benskin.graphql_spring_boot_tutorial.entities.Pet;
 import uk.co.benskin.graphql_spring_boot_tutorial.repositories.PetRepository;
 
 @Component
+@RequiredArgsConstructor
 public class Query implements GraphQLQueryResolver {
 
-    private PetRepository PetRepository;
-
-    public Query(PetRepository PetRepository) {
-        this.PetRepository = PetRepository;
-    }
+    private final PetRepository PetRepository;
 
     public Iterable<Pet> pets() {
         return PetRepository.findAll();
